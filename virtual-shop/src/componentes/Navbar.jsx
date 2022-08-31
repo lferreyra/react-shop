f0cc00d164142b851769e4e3a3cf1acb7c03a0cb
Lucas Ariel Ferreyra <lucas.ferreyra@gmail.com>
2022-08-31 12:53:48 -0300

    se modificó el boton de carrito de compras

--- virtual-shop/src/componentes/Navbar.jsx
+++ virtual-shop/src/componentes/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from ".//imagenes/logoDANUCCI.png";
-import Carrito from "./Menu/basket2-fill.svg";
+import Carrito from "./imagenes/carrito.png";
 
 const Navbar = () => {
     return(
@@ -22,9 +22,7 @@ const Navbar = () => {
                     <a className="nav-link link_header" href="#">Contacto</a>
                 </li>
                 <li >
-                <svg width="20" alt="carrito"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-">
-                <path href="#" strokeLinecap="round" strokeLinejoin="round"  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                </svg>
+                <a className="nav-link link_header" href=""><img src={Carrito} width="20" alt="Carrito" /></a>
                 </li>
                 
             </ul>
