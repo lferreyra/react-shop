728389a738ead957d98ee80623bb6706e291ef4e
Lucas Ariel Ferreyra <lucas.ferreyra@gmail.com>
2022-09-15 21:06:33 -0300

    subiendo ItemDetail a CLASE-ITEMDETAIL (branch)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
+import React from 'react';
 import './App.css';
 import ItemDetailContainer from './componentes/ItemDetailContainer';
-
+import ItemListContainer from './componentes/ItemListContainer';
+import Navbar from './componentes/Navbar';
+import Destacados from "./componentes/Destacados"
+import Footer from "./componentes/footer"
+import Promoapp from "./componentes/Promoapp"
 
 
 
 function App() {
 
   return (
+    <div className='container-fluid'>
+ <Navbar />
+ <Destacados />
+ <ItemListContainer />
+ <ItemDetailContainer />
 
-      <div className='container-fluid  fondo_amarillo'>     
-          <h3 className="text-center">Disfruta de nuestros productos</h3>
-          <ItemDetailContainer />
-         
-          
-      
 
-   </div>
-  );
-}
+<Promoapp />
+<Footer />
 
+
+</div>
+)}
 export default App;
 
 
--- src/componentes/tailwind.config.js
+++ src/componentes/tailwind.config.js
@@ -5,4 +5,36 @@ module.exports = {
       extend: {},
     },
     plugins: [],
-  }
+ 
+    important: true,
+    // Active dark mode on class basis
+    darkMode: "class",
+    i18n: {
+      locales: ["en-US"],
+      defaultLocale: "en-US",
+    },
+    purge: {
+      content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+      // These options are passed through directly to PurgeCSS
+    },
+    theme: {
+      extend: {
+        backgroundImage: (theme) => ({
+          check: "url('/icons/check.svg')",
+          landscape: "url('/images/landscape/2.jpg')",
+        }),
+      },
+    },
+    variants: {
+      extend: {
+        backgroundColor: ["checked"],
+        borderColor: ["checked"],
+        inset: ["checked"],
+        zIndex: ["hover", "active"],
+      },
+    },
+    plugins: [],
+    future: {
+      purgeLayersByDefault: true,
+    },
+  };
