29be2a74a6539a64385ebda98e26577851dde5d9
Lucas Ariel Ferreyra <lucas.ferreyra@gmail.com>
2022-09-15 21:22:29 -0300

    subiendo ITEMDETAIL

--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -8,19 +8,19 @@ const Navbar = () => {
         <div className="container"> 
             <ul className="nav d-flex align-items-center">
                 <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#"><img src={logo} width="48" alt="Logo" /></a> 
+                    <a className="nav-link active" aria-current="page" href="./"><img src={logo} width="48" alt="Logo" /></a> 
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link link_header" href="#">Pedí tu Danucci</a>
+                    <a className="nav-link link_header" href="./menu">Pedí tu Danucci</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link link_header" href="#">Catalogo</a>
+                    <a className="nav-link link_header" href="./menu/detail">Catalogo</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link link_header" href="#">Promos</a>
+                    <a className="nav-link link_header" href="./menu/detailContainer">Promos</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link link_header" href="#">Contacto</a>
+                    <a className="nav-link link_header" href="./cupones">Contacto</a>
                 </li>
                 <li >
                 <CartWidget />
